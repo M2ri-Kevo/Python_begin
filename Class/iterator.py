be59cb1b8c2d8ca7d69546693ac8f94308959869
M2ri-Kevo <muturikelvinofficial@gmail.com>
2022-11-03 06:38:15 +0000

    Iterators are here

--- Class/iterator.py
+++ Class/iterator.py
@@ -2,27 +2,6 @@ class MyNumbers:
     def __iter__(self):
         self.a = 1
         return self
-
-    def __next__(self):
-        x = self.a
-        self.a += 1
-        return x
-
-myclass = MyNumbers()
-myiter = iter(myclass)
-
-print(next(myiter))
-print(next(myiter))
-print(next(myiter))
-print(next(myiter))
-print(next(myiter))
-print(next(myiter))
-print(next(myiter))
-
-class MyNumbers:
-    def __iter__(self):
-        self.a = 1
-        return self
     def __next__(self):
         if self.a <= 100:
             x = self.a
